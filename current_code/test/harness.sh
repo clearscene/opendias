66e64a7da8d943b340a9e1ba8ebe2daed2545c08
wbooth <wbooth>
2010-04-24 14:14:06 +0000

    historic work - checking so not lost

--- current_code/test/harness.sh
+++ current_code/test/harness.sh
@@ -2,15 +2,15 @@
 
 # Setup
 DATE=`date +%Y-%b-%d_%H:%M`
-echo -en "<html><style>.none {background-color: none}\n .ok {background-color: #ccFFcc;}\n .fail {background-color: #FFCCCC;}</style><body><h1>Regression Tests, last run: $DATE</h1><h2>Results</h2>\n" > test/lastRegression/index.html
-echo -en "<table cellpadding=1 cellspacing=1 border=1><tr><th>Result</th><th>Test</th><th colspan=2>Memory</th><th colspan=2>Test Log</th><th colspan=2>App Log</th></tr>\n" >> test/lastRegression/index.html
-
 runTests="";
 testCount="";
 passCount="";
 failCount="";
-outputDir="test/lastRegression";
-PYTHONPATH="test/regressionTests/";
+outputDir="results/resultsFiles";
+PYTHONPATH="testCases\/";
+echo -en "<html><style>.none {background-color: none}\n .ok {background-color: #ccFFcc;}\n .fail {background-color: #FFCCCC;}</style><body><h1>Regression Tests, last run: $DATE</h1><h2>Results</h2>\n" > $outputDir/index.html
+echo -en "<table cellpadding=1 cellspacing=1 border=1><tr><th>Result</th><th>Test</th><th colspan=2>Memory</th><th colspan=2>Test Log</th><th colspan=2>App Log</th></tr>\n" >> $outputDir/index.html
+
 
 RECORD=""
 SKIPMEMORY=""
@@ -46,22 +46,23 @@ fi
 # Loop over each request.
 for requested in $runTests; do
   # Find all tests that match this request (eg. "1*", given "1_1_General" and "1_2_GeneralFiles")
-  for TEST in `eval find test/regressionTests/ -maxdepth 1 -type f -name $requested | grep -v utilsLib.py | grep -v "cvsignore" | sort`; do
+  for TEST in `eval find $PYTHONPATH -maxdepth 1 -type f -name $requested | sort`; do
     # Create results area and cleanup any old test results
-    mkdir -p $outputDir/$TEST
-    rm -rf $outputDir/$TEST/*
+    TESTCASENAME=`echo "$TEST" | sed -e "s/$PYTHONPATH//" `
+    mkdir -p $outputDir/$TESTCASENAME
+    rm -rf $outputDir/$TESTCASENAME/*
     rm -rf /tmp/ldtp-$USER/*
 
     # Create fixed startup environment
     # backup current environment
     if [ -d ~/.openDIAS/ ]; then
-      echo Too hot for me! directory $HOME/.openDIAS exists. Cowidly resusing to destroy it.
+      echo Too hot for me! directory $HOME/.openDIAS exists. Cowidly refusing to destroy it.
       exit
     fi
 
     # Build new environment
-    if [ -f $TEST.inputs/homeDir ]; then
-      cp -r $TEST.inputs/homeDir ~/.openDIAS
+    if [ -f $PYTHONPATH/inputs/$TESTCASENAME/homeDir ]; then
+      cp -r $PYTHONPATH/inputs/$TESTCASENAME/homeDir ~/.openDIAS
     fi
 
     # Reset test result vars
@@ -85,22 +86,22 @@ for requested in $runTests; do
 
       # memory log
       if [ "$SKIPMEMORY" == "" ]; then
-        mv $outputDir/valgrind.out $outputDir/$TEST/valgrind.out
+        mv $outputDir/valgrind.out $outputDir/$TESTCASENAME/valgrind.out
         # parse out changeable content
-        sed -f test/valgrindUnify.sed < $outputDir/$TEST/valgrind.out > $outputDir/$TEST/valgrind4Compare.out
+        sed -f config/valgrindUnify.sed < $outputDir/$TESTCASENAME/valgrind.out > $outputDir/$TESTCASENAME/valgrind4Compare.out
         if [ "$GENERATE" == "Y" ]; then
-          if [ ! -e ${TEST}.result ]; then
-            mkdir -p ${TEST}.result
+          if [ ! -e $PYTHONPATH/results/$TESTCASENAME ]; then
+            mkdir -p $PYTHONPATH/results/$TESTCASENAME
           fi
-          cp $outputDir/$TEST/valgrind4Compare.out ${TEST}.result/valgrind.out
+          cp $outputDir/$TESTCASENAME/valgrind4Compare.out $PYTHONPATH/results/$TESTCASENAME/valgrind.out
         fi
-        MEM_RES="<td class='none'><a href='$TEST/valgrind.out'>actual</a></td>"
-        diff -ydN ${TEST}.result/valgrind.out $outputDir/$TEST/valgrind4Compare.out > $outputDir/$TEST/valgrindDiff.out
+        MEM_RES="<td class='none'><a href='$TESTCASENAME/valgrind.out'>actual</a></td>"
+        diff -ydN $PYHTONPATH/results/$TESTCASENAME/valgrind.out $outputDir/$TESTCASENAME/valgrind4Compare.out > $outputDir/$TESTCASENAME/valgrindDiff.out
         if [ "$?" == "0" ]; then
-          rm $outputDir/$TEST/valgrindDiff.out
+          rm $outputDir/$TESTCASENAME/valgrindDiff.out
           MEM_RES="$MEM_RES<td class='ok'>OK</td>"
         else
-          MEM_RES="$MEM_RES<td><a href='$TEST/valgrindDiff.out'>diff</a>&nbsp;|&nbsp;<a href='../../${TEST}.result/valgrind.out'>expected</a></td>"
+          MEM_RES="$MEM_RES<td><a href='$TESTCASENAME/valgrindDiff.out'>diff</a>&nbsp;|&nbsp;<a href='../../$PYTHONPATH/results/$TESTCASENAME/valgrind.out'>expected</a></td>"
           RES=1
         fi
       else
@@ -108,39 +109,39 @@ for requested in $runTests; do
       fi
 
       # test log
-      mv /tmp/ldtp-$USER/* $outputDir/$TEST/
-      cp $outputDir/$TEST/*.xml $outputDir/$TEST/index.out
+      mv /tmp/ldtp-$USER/* $outputDir/$TESTCASENAME/
+      cp $outputDir/$TESTCASENAME/*.xml $outputDir/$TESTCASENAME/index.out
       if [ "$GENERATE" == "Y" ]; then
-        if [ ! -e ${TEST}.result ]; then
-          mkdir -p ${TEST}.result
+        if [ ! -e $PYTHONPATH/results/$TESTCASENAME ]; then
+          mkdir -p $PYTHONPATH/results/$TESTCAENAME
         fi
-        cp $outputDir/$TEST/index.out ${TEST}.result/index.out
+        cp $outputDir/$TESTCASENAME/index.out $PYTHONPATH/results/$TESTCASENAME/index.out
       fi
-      TEST_RES="<td class='none'><a href='$TEST/index.out'>actual</a></td>"
-      diff -ydN ${TEST}.result/index.out $outputDir/$TEST/index.out > $outputDir/$TEST/testLogDiff.out
+      TEST_RES="<td class='none'><a href='$TESTCASENAME/index.out'>actual</a></td>"
+      diff -ydN $PYTHONPATH/results/$TESTCASENAME/index.out $outputDir/$TESTCASENAME/index.out > $outputDir/$TESTCASENAME/testLogDiff.out
       if [ "$?" == "0" ]; then
-        rm $outputDir/$TEST/testLogDiff.out
+        rm $outputDir/$TESTCASENAME/testLogDiff.out
         TEST_RES="$TEST_RES<td class='ok'>OK</td>"
       else
-        TEST_RES="$TEST_RES<td><a href='$TEST/testLogDiff.out'>diff</a>&nbsp;|&nbsp;<a href='../../${TEST}.result/index.out'>expected</a></td>"
+        TEST_RES="$TEST_RES<td><a href='$TESTCASENAME/testLogDiff.out'>diff</a>&nbsp;|&nbsp;<a href='../../$PYHTONPATH/results/$TESTCASENAME/index.out'>expected</a></td>"
         RES=1
       fi
 
       # app log
-      cp $outputDir/appLog.out $outputDir/$TEST/appLog.out
+      cp $outputDir/appLog.out $outputDir/$TESTCASENAME/appLog.out
       if [ "$GENERATE" == "Y" ]; then
-        if [ ! -e ${TEST}.result ]; then
-          mkdir -p ${TEST}.result
+        if [ ! -e $PYTHONPATH/results/$TESTCASENAME ]; then
+          mkdir -p $PYTHONPATH/results/$TESTCAENAME
         fi
-        cp $outputDir/$TEST/appLog.out ${TEST}.result/appLog.out
+        cp $outputDir/$TESTCASENAME/appLog.out $PYTHONPATH/results/$TESTCASENAME/appLog.out
       fi
-      APP_RES="<td class='none'><a href='$TEST/appLog.out'>actual</a></td>"
-      diff -ydN ${TEST}.result/appLog.out $outputDir/$TEST/appLog.out > $outputDir/$TEST/appLogDiff.out
+      APP_RES="<td class='none'><a href='$TESTCASENAME/appLog.out'>actual</a></td>"
+      diff -ydN $PYTHONPATH/results/$TESTCASENAME/appLog.out $outputDir/$TESTCASENAME/appLog.out > $outputDir/$TESTCASENAME/appLogDiff.out
       if [ "$?" == "0" ]; then
-        rm $outputDir/$TEST/appLogDiff.out
+        rm $outputDir/$TESTCASENAME/appLogDiff.out
         APP_RES="$APP_RES<td class='ok'>OK</td>"
       else
-        APP_RES="$APP_RES<td><a href='$TEST/appLogDiff.out'>diff</a>&nbsp;|&nbsp;<a href='../../${TEST}.result/appLog.out'>expected</a></td>"
+        APP_RES="$APP_RES<td><a href='$TESTCASENAME/appLogDiff.out'>diff</a>&nbsp;|&nbsp;<a href='../../$PYTHONPATH/results/$TESTCASENAME/appLog.out'>expected</a></td>"
         RES=1
       fi
 
@@ -163,7 +164,7 @@ for requested in $runTests; do
     fi
 
     # Output result line
-    echo "<td><a href='$TEST/'>$TEST</a></td>" >> $outputDir/index.html
+    echo "<td><a href='$TESTCASENAME/'>$TESTCASENAME</a></td>" >> $outputDir/index.html
     echo $MEM_RES >> $outputDir/index.html
     echo $TEST_RES >> $outputDir/index.html
     echo $APP_RES >> $outputDir/index.html
@@ -182,7 +183,7 @@ echo -en "</table>" >> $outputDir/index.html
 testCount=`echo -n $testCount | wc -m`
 passCount=`echo -n $passCount | wc -m`
 failCount=`echo -n $failCount | wc -m`
-echo -en "<hr /><p><strong>Tests: $testCount   Passed: $passCount     Failed: $failCount </strong></p>" >> $outputDir/index.html
+echo -en "<hr /><p><strong>Tests: $testCount<br />Passed: $passCount<br />Failed: $failCount </strong></p>" >> $outputDir/index.html
 
 # Finish off
 echo -en "</body></html>\n" >> $outputDir/index.html
