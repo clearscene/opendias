03e91a8e391d9eb8f819212651f1105b18fa4fb8
Wayne Booth <wbooth@clearscene.net>
2012-02-03 13:13:15 +0000

    add field names to the SQL

--- current_code/test/regressionTests/inputs/8_2_doc_detail/doc.sql
+++ current_code/test/regressionTests/inputs/8_2_doc_detail/doc.sql
@@ -1,3 +1,3 @@
 BEGIN TRANSACTION;
-INSERT INTO docs VALUES(1,1,'This is the OCR text.',8,3509,2519,300,2010,12,31,'2011-01-02T21:13:04.393946Z',2,'Test Title',1,0);
+INSERT INTO docs (docid, doneocr, ocrtext, depth, lines, ppl, resolution, docdatey, docdatem, docdated, entrydate, filetype, title, pages, actionrequired) VALUES (1,1,'This is the OCR text.',8,3509,2519,300,2010,12,31,'2011-01-02T21:13:04.393946Z',2,'Test Title',1,0);
 COMMIT;
