6c0dc7a2cd14220e8a75147c0820e3586a164b5e
Wayne Booth <wbooth@clearscene.net>
2012-02-04 15:09:16 +0000

    search test - missing tags atm

---
+++ current_code/test/regressionTests/3_2_doclist_search.pm
@@ -0,0 +1,121 @@
+package regressionTests::3_2_doclist_search;
+
+use lib qw( regressionTests/lib );
+use DBI;
+use standardTests;
+use Data::Dumper;
+use strict;
+
+sub testProfile {
+  return {
+    valgrind => 1,
+    client => 0,
+  }; 
+} 
+
+sub test {
+
+  $Data::Dumper::Indent = 1;
+  $Data::Dumper::Sortkeys = 1;
+
+  my %data = (
+    action => 'docFilter',
+    page => '1',
+    range => '4',
+    sortfield => '3', # date
+    sortorder => '1',
+    isActionRequired => 'false',
+    textSearch => undef,
+    startDate => undef,
+    endDate => undef,
+    tags => undef,
+  );
+
+
+  # All rows
+  o_log( "Full list" );
+  $data{subaction} = 'count';
+  o_log( Dumper( directRequest( \%data ) ) );
+  $data{subaction} = 'fullList';
+  o_log( Dumper( directRequest( \%data ) ) );
+
+
+  # Action Required
+  o_log( "With Action Required" );
+  $data{isActionRequired} = 'true';
+  $data{subaction} = 'count';
+  o_log( Dumper( directRequest( \%data ) ) );
+  $data{subaction} = 'fullList';
+  o_log( Dumper( directRequest( \%data ) ) );
+  $data{isActionRequired} = 'false';
+
+
+  # Text search - no hits
+  o_log( "Text search with no results" );
+  $data{textSearch} = 'no entry';
+  $data{subaction} = 'count';
+  o_log( Dumper( directRequest( \%data ) ) );
+  $data{subaction} = 'fullList';
+  o_log( Dumper( directRequest( \%data ) ) );
+  $data{textSearch} = undef;
+
+
+  # Text search - title hits
+  o_log( "Text search - matches on title" );
+  $data{textSearch} = 'titletext';
+  $data{subaction} = 'count';
+  o_log( Dumper( directRequest( \%data ) ) );
+  $data{subaction} = 'fullList';
+  o_log( Dumper( directRequest( \%data ) ) );
+  $data{textSearch} = undef;
+
+
+  # Text search - ocr hits
+  o_log( "Text search - matches on OCR text" );
+  $data{textSearch} = 'is ocr text';
+  $data{subaction} = 'count';
+  o_log( Dumper( directRequest( \%data ) ) );
+  $data{subaction} = 'fullList';
+  o_log( Dumper( directRequest( \%data ) ) );
+  $data{textSearch} = undef;
+
+
+  # Date filter
+  o_log( "Date Filter" );
+  $data{startDate} = '2010-12-31';
+  $data{endDate} = '2011-01-01';
+  $data{subaction} = 'count';
+  o_log( Dumper( directRequest( \%data ) ) );
+  $data{subaction} = 'fullList';
+  o_log( Dumper( directRequest( \%data ) ) );
+
+
+  # Compound filter
+  o_log( "Date Filter date/text" );
+  $data{startDate} = '2010-12-31';
+  $data{endDate} = '2011-01-01';
+  $data{textSearch} = 'Test Title';
+  $data{subaction} = 'count';
+  o_log( Dumper( directRequest( \%data ) ) );
+  $data{subaction} = 'fullList';
+  o_log( Dumper( directRequest( \%data ) ) );
+  $data{textSearch} = undef;
+
+
+  # Compound filter
+  o_log( "Date Filter date/action" );
+  $data{startDate} = '2010-12-31';
+  $data{endDate} = '2011-01-01';
+  $data{isActionRequired} = 'true';
+  $data{subaction} = 'count';
+  o_log( Dumper( directRequest( \%data ) ) );
+  $data{subaction} = 'fullList';
+  o_log( Dumper( directRequest( \%data ) ) );
+  $data{isActionRequired} = 'false';
+
+
+  return 0;
+}
+
+return 1;
+
