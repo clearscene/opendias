8301a5095829999377e1e0ead2536fe33a8b7d1c
Wayne Booth <wbooth@clearscene.net>
2012-02-05 15:10:16 +0000

    add a get scanner list API test

---
+++ current_code/test/regressionTests/3_3_getScannerList.pm
@@ -0,0 +1,35 @@
+package regressionTests::3_3_getScannerList;
+
+use lib qw( regressionTests/lib );
+use DBI;
+use standardTests;
+use Data::Dumper;
+use strict;
+
+sub testProfile {
+  return {
+    valgrind => 1,
+    client => 0,
+  }; 
+} 
+
+sub test {
+
+  $Data::Dumper::Indent = 1;
+  $Data::Dumper::Sortkeys = 1;
+
+  my %data = (
+    action => 'getScannerList',
+  );
+
+
+  # All rows
+  o_log( "Scanner List" );
+  o_log( Dumper( directRequest( \%data ) ) );
+
+
+  return 0;
+}
+
+return 1;
+
