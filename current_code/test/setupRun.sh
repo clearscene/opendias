09328479d3d9dc09339b39418f759b4ecbc48ffb
Wayne Booth <wbooth@clearscene.net>
2012-02-06 00:15:31 +0000

    update testplan to account for recent new tests.

--- current_code/test/setupRun.sh
+++ current_code/test/setupRun.sh
@@ -116,10 +116,10 @@ if [ "$NOBUILD" == "" ]; then
   cd ../
   if [ "$SKIPCOVERAGE" == "-c" ]; then
     echo " (without coverage) ..."
-    ./configure -C CFLAGS=' -g3 -O0 ' &> test/results/buildLog2.out
+    ./configure -C CFLAGS=' -g -O ' &> test/results/buildLog2.out
   else
     echo " (with coverage) ..."
-    ./configure -C CFLAGS=' -g3 -O0 --coverage' LIBS='-lgcov' &> test/results/buildLog2.out
+    ./configure -C CFLAGS=' -g -O --coverage' LIBS='-lgcov' &> test/results/buildLog2.out
   fi
   cd test
   # unfortunatly bash cannot support "&>>" - yet!
