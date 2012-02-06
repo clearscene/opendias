7bbb27ce3d8d46bc5c3cc5ab66cbec880d3603b6
Wayne Booth <wbooth@clearscene.net>
2012-02-06 10:05:20 +0000

    git mv admin overhead

---
+++ current_code/test/regressionTests/3_a_scanning.pm
@@ -0,0 +1,38 @@
+package regressionTests::3_5_scanning;
+
+use lib qw( regressionTests/lib );
+use DBI;
+use standardTests;
+use Data::Dumper;
+use strict;
+
+sub testProfile {
+  return {
+    valgrind => 1,
+    client => 0,
+  }; 
+} 
+
+sub test {
+
+  $Data::Dumper::Indent = 1;
+  $Data::Dumper::Sortkeys = 1;
+
+  my %data = (
+    action => 'doScan',
+    deviceid => 'test:0',
+    format => 'Grey Scale',
+    pages => 1,
+    resolution => '300',
+    ocr => '-',
+    pagelength => '100',
+  );
+
+  # Entry DB
+  o_log( "No Rows = " . Dumper( directRequest( \%data ) ) );
+
+  return 0;
+}
+
+return 1;
+
