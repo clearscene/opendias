91c4094cd9a918700002564f6b91e0c3e4ec0ab6
Wayne Booth <wbooth@clearscene.net>
2012-02-14 17:54:22 +0000

    ensure the current set of testing is not giving false positives by rejecting txns based on already defined duff fields.

--- current_code/test/regressionTests/3_b_validation.pm
+++ current_code/test/regressionTests/3_b_validation.pm
@@ -21,16 +21,16 @@ sub test {
           },
     doScan => { 
           deviceid => 'text',
-          format => 'text',
-          pages => 'int',
-          resolution => 'int',
-          ocr => 'text',
-          pageLength => 'int',
+          format => 'grey scale',
+          pages => '1',
+          resolution => '300',
+          ocr => 'lang',
+          pagelength => '100',
           },
     docFilter => { 
           subaction => [ 'count', 'fullList' ],
           textSearch => 'text',
-          isActionRequired => 'bool',
+          isActionRequired => 'boolean',
           startDate => 'date',
           endDate => 'date',
           tags => 'text',
@@ -74,15 +74,19 @@ sub test {
   # No data
   o_log( "no post data = " . Dumper( directRequest( { } ) ) );
 
+
   # Unknown 'action'
   o_log( "Unknown action = " . Dumper( directRequest( { action => 'rumplestilskin' } ) ) );
+
 
   # Try all API actions, with zero supporting fields - expect 'error' response
   foreach my $action (sort {$a cmp $b} (keys %calls)) {
     o_log( "$action = " . Dumper( directRequest( { action => $action } ) ) );
   }
 
-  # Try all API actions that require a subaction, send an unknown sub action - expect 'error' response
+
+  # Try all API actions that require a subaction, send an unknown 
+  # sub action - expect 'error' response
   foreach my $action (sort {$a cmp $b} (keys %calls)) {
     next unless exists $calls{$action}->{subaction};
     my %local_details = %{$calls{$action}};
@@ -91,7 +95,9 @@ sub test {
     o_log( "Unknown subaction on action of $action = " . Dumper( directRequest( { action => $action, subaction => 'rumplestilskin', %local_details } ) ) );
   }
 
-  # Try all API actions that require a subaction, send a valid sub subaction, but no supporting fields - expect 'error' response
+
+  # Try all API actions that require a subaction, send a valid sub subaction, 
+  # but no supporting fields - expect 'error' response
   foreach my $action (sort {$a cmp $b} (keys %calls)) {
     next unless exists $calls{$action}->{subaction};
     foreach my $subaction ( @{$calls{$action}->{subaction}} ) {
@@ -99,44 +105,80 @@ sub test {
     }
   }
 
-  # Try all API actions that require a subaction, send a valid sub subaction, and all subfields, but include a rogue field.
+
+  # Try all API actions, send a valid sub subaction, 
+  # and all subfields, but include a rogue field.
   foreach my $action (sort {$a cmp $b} (keys %calls)) {
     my %local_details = %{$calls{$action}};
     replaceWithValues(\%local_details);
     if( exists $local_details{subaction} ) {
       delete $local_details{subaction};
       foreach my $subaction ( @{$calls{$action}->{subaction}} ) {
-        o_log( "actions of $action / $subaction = " . Dumper( directRequest( { action => $action, subaction => $subaction, rogue_field => 'evil', %local_details } ) ) );
+        o_log( "rogue field in actions of $action / $subaction = " . Dumper( directRequest( { action => $action, subaction => $subaction, rogue_field => 'evil', %local_details } ) ) );
       }
     }
     else {
-      o_log( "actions of $action = " . Dumper( directRequest( { action => $action, rogue_field => 'evil', %local_details } ) ) );
+      o_log( "rogue field in actions of $action = " . Dumper( directRequest( { action => $action, rogue_field => 'evil', %local_details } ) ) );
     }
   }
+
+
+#  # Try all API actions that require a subaction, send a valid sub subaction, 
+#  # and all subfields, but include a rogue field.
+#  foreach my $action (sort {$a cmp $b} (keys %calls)) {
+#    foreach my $field (sort {$a cmp $b} (keys %{$calls{$action}})) {
+#      next if $field eq "subaction"; # we've already tested an incorrect subaction
+#      my %local_details = %{$calls{$action}};
+#      replaceWithValues(\%local_details, $field);
+#      if( exists $local_details{subaction} ) {
+#        delete $local_details{subaction};
+#        foreach my $subaction ( @{$calls{$action}->{subaction}} ) {
+#          o_log( "field $field with duff data, $action / $subaction = " . Dumper( directRequest( { action => $action, subaction => $subaction, %local_details } ) ) );
+#        }
+#      }
+#      else {
+#        o_log( "field $field with duff data, $action = " . Dumper( directRequest( { action => $action, %local_details } ) ) );
+#      }
+#    }
+#  }
+
 
   return 0;
 }
 
 sub replaceWithValues {
-  my( $hashRef, ) = @_;
+  my( $hashRef, $isBad, ) = @_;
+  $isBad = '' unless $isBad;
   foreach my $key (keys %$hashRef) {
+    next if $key eq "subaction";
     if($hashRef->{$key} eq 'int') {
-      $hashRef->{$key} = 12345;
+      $hashRef->{$key} = 1234;
+      $hashRef->{$key} = 123499999 if $isBad eq $key;
     }
     elsif($hashRef->{$key} eq 'text') {
       $hashRef->{$key} = 'some text';
+      $hashRef->{$key} = '' if $isBad eq $key;
     }
     elsif($hashRef->{$key} eq 'uuid') {
       $hashRef->{$key} = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa';
+      $hashRef->{$key} = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaZaaaaa' if $isBad eq $key;
     }
     elsif($hashRef->{$key} eq 'date') {
-      $hashRef->{$key} = '2012-02-01';
+      $hashRef->{$key} = '2012-12-31';
+      $hashRef->{$key} = '12-31-2012' if $isBad eq $key;
+    }
+    elsif($hashRef->{$key} eq 'lang') {
+      $hashRef->{$key} = 'eng';
+      $hashRef->{$key} = 'ZZZ' if $isBad eq $key;
     }
     elsif($hashRef->{$key} eq 'boolean') {
       $hashRef->{$key} = 'true';
+      $hashRef->{$key} = 'yes' if $isBad eq $key;
     }
-
-    
+    else {
+      # No mapping - then value must be hard coded
+      $hashRef->{$key} = '' if $isBad eq $key;
+    }
   }
 }
 
