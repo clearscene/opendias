0fb708eb609f2561cf695c51e35bcadedaffd1e8
Wayne Booth <wbooth@clearscene.net>
2012-03-23 07:17:04 +0000

    remove the 'locking field' as this is not going to be used now.

--- current_code/sql/openDIAS.sqlite3.dmp.v6.sql
+++ current_code/sql/openDIAS.sqlite3.dmp.v6.sql
@@ -17,5 +17,19 @@ CREATE TABLE doc_links (
 CREATE INDEX doc_links_linkid_idx
 ON doc_links(docid ASC);
 
+
+CREATE TEMPORARY TABLE t1_backup("status" INTEGER, "value" INTEGER, "client_id" CHAR(36));
+INSERT INTO t1_backup SELECT status, value, client_id FROM scan_progress;
+DROP TABLE scan_progress;
+CREATE TABLE scan_progress (
+    "status" INTEGER NOT NULL DEFAULT (0),
+    "value" INTEGER,
+    "client_id" CHAR(36) NOT NULL DEFAULT (' ')
+);
+INSERT INTO scan_progress SELECT status, value, client_id FROM t1_backup;
+CREATE UNIQUE INDEX "client_id" on "scan_progress" (client_id ASC);
+DROP TABLE t1_backup;
+
+
 COMMIT;
 
