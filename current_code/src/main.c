b0f68bc8aa04425b40367076e304f5374405c142
Steve Meifert <skm42@users.sf.net>
2012-03-28 17:50:08 +0100

    no change

--- current_code/src/main.c
+++ current_code/src/main.c
@@ -53,10 +53,12 @@ int setup (char *configFile) {
   LOG_DIR = o_strdup("/var/log/opendias");
 
   // Get 'DB' location
-  if (configFile != NULL)
+  if (configFile != NULL) {
     conf = configFile;
-  else
+  } else {
     conf = DEFAULT_CONF_FILE;
+  }
+
 
   o_log(INFORMATION, "Using config file: %s", conf);
   if( 0 == load_file_to_memory(conf, &location) ) {
@@ -203,7 +205,7 @@ void daemonize(char *rundir, char *pidfile) {
  
     /* Child continues */
 
-    (void)umask(027); /* Set file permissions 750 */
+    (void)umask(027); /* Set file permissions 750 ? that's 640 but that's fine */
  
     /* Get a new process group */
     sid = setsid();
