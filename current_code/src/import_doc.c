1ea0c5a46253d202b1240029b814e44c5209c1fe
Wayne Booth <wbooth@clearscene.net>
2012-05-02 07:32:56 +0100

    unify the creation of ocrText and thumbnail images for imported docs.

--- current_code/src/import_doc.c
+++ current_code/src/import_doc.c
@@ -18,7 +18,11 @@
 
 
 #include "config.h"
-#include "import_doc.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "main.h"
 #include "dbaccess.h"
 #include "utils.h"
@@ -29,52 +33,51 @@
 #ifdef CAN_PDF
 #include "pdf_plug.h"
 #endif // CAN_PDF //
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
 #ifdef CAN_OCR
 #include "imageProcessing.h"
 #endif // CAN_OCR //
 
+#include "import_doc.h"
+
 char *uploadfile(char *filename, char *ftype) {
 
   int itype = PLACE_HOLDER;
-  char *to_name, *tmp, *ocrText = NULL;
+  char *to_name, *datafile, *ocrText = NULL, *thumbext = NULL, *tmp;
   const char *docid;
 
   // Save Record
   o_log(DEBUGM, "Saving doc import record");
 
+  datafile = o_printf("/tmp/%s.dat", filename);
 
   // --------------------------------------
-  if(0==strcmp("PDF", ftype)) {
+  if( 0 == strcmp("PDF", ftype) ) {
+    char *outfile;
     itype = PDF_FILETYPE;
-
-    tmp = o_printf("/tmp/%s.dat", filename);
-    get_image_from_pdf( tmp );
-    ocrText = get_text_from_pdf( tmp );
-    free(tmp);
-
-    if( ocrText == NULL ) {
-      ocrText = o_strdup("fdsdfsdfsdf");
-    }
+#ifdef CAN_PDF
+    outfile = o_printf("/tmp/%s.thumb", filename);
+    get_image_from_pdf( datafile, outfile ); // pdf_plug.cc
+    ocrText = get_text_from_pdf( datafile ); // pdf_plug.cc
+    free(outfile);
+    thumbext = o_strdup("jpg");
+#endif // CAN_PDF //
   }
 
   // --------------------------------------
-  else if(0==strcmp("ODF", ftype)) {
+  else if( 0 == strcmp("ODF", ftype) ) {
+    char *outfile;
     itype = ODF_FILETYPE;
-    tmp = o_printf("/tmp/%s.dat", filename);
 #ifdef CAN_READODF
-    ocrText = get_odf_Text(tmp); // read_odf.c 
+    outfile = o_printf("/tmp/%s.thumb", filename);
+    get_odf_Thumb( datafile, outfile );
+    ocrText = get_odf_Text( datafile ); // read_odf.c 
+    free(outfile);
+    thumbext = o_strdup("png");
 #endif // CAN_READODF //
-    if(ocrText == NULL)
-      ocrText = o_strdup("--text could not be extracted--");
-    free(tmp);
   }
 
   // --------------------------------------
-  else if(0==strcmp("jpg", ftype)) {
+  else if( 0 == strcmp("jpg", ftype) ) {
     itype = JPG_FILETYPE;
 //    char *pic = NULL;
 //    ocrText = getTextFromImage((const unsigned char *)pic, 1, 1, 1);
@@ -82,23 +85,39 @@ char *uploadfile(char *filename, char *ftype) {
   }
 
   // --------------------------------------
-  if(itype == PLACE_HOLDER) {
+  else {
     o_log(ERROR, "unknown file type.");
+    free(datafile);
     return NULL;
   }
+
+  if(ocrText == NULL) {
+    ocrText = o_strdup("--text could not be extracted--");
+  }
+  free(datafile);
 
   // Save the record to the DB
   docid = addNewFileDoc(itype, ocrText); //ocrText get freed in this method
 
-  // Docs that are not scanned, are stored with no "_x" postfix, but any thumbnails - are.
-  to_name = o_printf("%s/scans/%s", BASE_DIR, docid);
+  // Move the datafile to the file store location
+  to_name = o_printf("%s/scans/%s", BASE_DIR, docid); // imported docs, are stored with no "_x" postfix.
   addFileExt(&to_name, itype);
-
   tmp = o_printf("/tmp/%s.dat", filename);
-
   fcopy(tmp, to_name);
   free(tmp);
   free(to_name);
+
+  // Move the datafile to the file store location
+  if( thumbext ) {
+    to_name = o_printf("%s/scans/%s_thumb.%s", BASE_DIR, docid, thumbext); // any thumbnails are postfixed with "_thumb"
+    tmp = o_printf("/tmp/%s.thumb", filename);
+    fcopy(tmp, to_name);
+    unlink(tmp);
+    free(tmp);
+    free(to_name);
+    free(thumbext);
+  }
+
 
   // Open the document for editing.
   tmp = o_printf("<html><HEAD><META HTTP-EQUIV=\"refresh\" CONTENT=\"0;URL=/opendias/docDetail.html?docid=%s\"></HEAD><body></body></html>", docid);
