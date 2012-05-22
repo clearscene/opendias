b65882f6a42bc2df79a7a24fa22c71238fb4cf77
Wayne Booth <wbooth@clearscene.net>
2012-05-22 07:26:33 +0100

    log the creation of a new upload handlename,

--- current_code/src/web_handler.c
+++ current_code/src/web_handler.c
@@ -160,6 +160,7 @@ static int iterate_post (void *coninfo_cls, enum MHD_ValueKind kind, const char 
           uuid_generate(uu);
           uuid_unparse(uu, fileid);
           data_struct->data = o_strdup(fileid);
+          o_log("DEBUGM, "Generated upload handlename %s", data_struct->data);
           free(fileid);
       }
       else {
@@ -197,7 +198,7 @@ static int iterate_post (void *coninfo_cls, enum MHD_ValueKind kind, const char 
         }
         close(fd);
       }
-    free(filename);
+      free(filename);
     }
 
     free(trimedData);
