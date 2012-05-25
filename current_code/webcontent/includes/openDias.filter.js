936225aecffd884e6e74b630f7a9c8ed71ddf7e2
Wayne Booth <wbooth@clearscene.net>
2012-05-25 18:04:57 +0100

    move doclist and filtering to a localised version.

--- current_code/webcontent/includes/openDias.filter.js
+++ current_code/webcontent/includes/openDias.filter.js
@@ -38,16 +38,16 @@ function getRecordCount() {
              type: "POST",
              success: function(data){
                if( $(data).find('error').text() ) {
-                 alert("Unable to get a filtered list:."+$(data).find('error').text());
+                 alert( LOCAL_unable_to_get_filtered_list + ": "+$(data).find('error').text());
                } else {
-                 $('#filterProgress').text( "Will return an estimated "+$(data).find('DocFilter').find('count').text()+" docs" );
+                 $('#filterProgress').text( LOCAL_will_return_estimated + " " + $(data).find('DocFilter').find('count').text() + " " + LOCAL_documents_abv );
                }
              },
              error: function( x, t, m ) {
                if(t=="timeout") {
-                 alert("[f001] Timeout while talking to the server.");
+                 alert("[f001] " + LOCAL_timeout_talking_to_server );
                } else {
-                 alert("[f001] Error while talking to the server: ".t);
+                 alert("[f001] " + LOCAL_error_talking_to_server + ": " + t);
                }
              },
            });
