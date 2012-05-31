5f347a742fc641435b38999b342338218512272d
Wayne Booth <wbooth@clearscene.net>
2012-05-31 07:32:37 +0100

    A full language pack, translated into '####' chars.

---
+++ contrib/test_localisation_files/includes/local/openDias.filter.js.resource.hh
@@ -0,0 +1,2 @@
+LOCAL_unable_to_get_filtered_list = '###### ## ### # ######## ####';
+LOCAL_will_return_estimated_x_docs = '#### ###### ## ######### %s ####';
---
+++ contrib/test_localisation_files/includes/local/openDias.getAccessDetails.js.resource.hh
@@ -0,0 +1,8 @@
+LOCAL_anyone_accessing_from = '###### ######### #### ';
+LOCAL_user_with_user_of = '#### #### ##### ## ';
+LOCAL_will_get_the = '#### ### ### ';
+LOCAL_role = ' ####';
+LOCAL_delete = '######';
+LOCAL_unable_to_get_access_info = '###### ## ### ###### ###########';
+LOCAL_anyone_accessing_from = '###### ######### #### ';
+LOCAL_user_with_logon_of = '#### #### ##### ## ';
