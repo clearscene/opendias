239788ab5d335543095511c96085db30e409f505
Wayne Booth <wbooth@clearscene.net>
2012-06-26 00:02:43 +0100

    add checks for cookie value (when changing language)

--- current_code/test/clientSideTesting/tests/homePageTest.js
+++ current_code/test/clientSideTesting/tests/homePageTest.js
@@ -30,17 +30,19 @@
     });
 
     // ------------------------------------------------
-		asyncTest('Page presented in broswers native language', 2, function() {
+		asyncTest('Page presented in broswers native language', 3, function() {
       console.log("1. Running: native language");
 
       equal($('#homeLink').text(), "Home", "Was the home link in the expeced language" );
       equal($('#home div p:first').text(), "Welcome to openDias. The Document Imaging Archive System.", "Was the welcome message expeced language" );
+      equal(getCookie('requested_lang'), null, "Was the cookie set as expected.");
+
       start();
 		});
 
 
     // ------------------------------------------------
-		asyncTest('Select Test Lang', 2, function() {
+		asyncTest('Select Test Lang', 3, function() {
       console.log("1. Running: update language dropdown");
 
       var langUpdated = 0;
@@ -49,6 +51,7 @@
         $ = window.frames[0].jQuery; 
         equal($('#homeLink').text(), "####", "Was the home link in the expeced language" );
         equal($('#home div p:first').text(), "####### ## openDias. ### Document Imaging Archive System.", "Was the welcome message updated to the expeced language" );
+        equal(getCookie('requested_lang'), 'hh', "Was the cookie set as expected.");
         start();
       });
       $('#language').val('hh').change();
@@ -62,7 +65,7 @@
 
 
     // ------------------------------------------------
-		asyncTest('Select German', 2, function() {
+		asyncTest('Select German', 3, function() {
       console.log("1. Running: update language dropdown");
 
       var langUpdated = 0;
@@ -71,6 +74,7 @@
         $ = window.frames[0].jQuery; 
         equal($('#homeLink').text(), "Zuhause", "Was the home link in the expeced language" );
         equal($('#home div p:first').text(), "Willkommen, um openDias. Die Document Imaging Archive System.", "Was the welcome message updated to the expeced language" );
+        equal(getCookie('requested_lang'), 'de', "Was the cookie set as expected.");
         start();
       });
       $('#language').val('de').change();
@@ -84,13 +88,23 @@
 
 
     // ------------------------------------------------
-		asyncTest('Reset language dropdown', 1, function() {
-      // delete any language cookie that may be present
-      var date = new Date();
-      date.setTime(date.getTime()-(24*60*60*1000));
-      var expires = "; expires="+date.toGMTString();
-      document.cookie = "requested_lang="+expires+"; path=/";
+		asyncTest('Reset language dropdown', 3, function() {
 
-      ok( 1, "Reset ok.");
-      start();
+      var langUpdated = 0;
+	    $q('#testframe').one('load', function() { 
+        langUpdated = 1;
+        $ = window.frames[0].jQuery; 
+        equal($('#homeLink').text(), "Home", "Was the home link in the expeced language" );
+        equal($('#home div p:first').text(), "Welcome to openDias. The Document Imaging Archive System.", "Was the welcome message expeced language" );
+        equal(getCookie('requested_lang'), null, "Was the cookie set as expected.");
+        start();
+      });
+      $('#language').val('--').change();
+      window.setTimeout( function() {
+        if( ! langUpdated ) {
+          ok( 0, "Did not load the home page.");
+          start();
+        }
+      }, 5000);
     });
+
