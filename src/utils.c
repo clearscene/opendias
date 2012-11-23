69bd76eb0898942287bd4198060af7f0db5a66e4
Wayne Booth <wbooth@clearscene.net>
2012-11-23 21:31:07 +0000

    reformat

--- src/utils.c
+++ src/utils.c
@@ -412,28 +412,29 @@ void o_concatf(char **mainStr, const char *fmt, ...) {
 }
 
 char *str2md5(const char *str, int length) {
-    int n;
-    MD5_CTX c;
-    unsigned char digest[16];
-    char *out = (char*)malloc(33);
+  int n;
+  MD5_CTX c;
+  unsigned char digest[16];
+  char *out = (char*)malloc(33);
 
-    MD5_Init(&c);
+  MD5_Init(&c);
 
-    while (length > 0) {
-        if (length > 512) {
-            MD5_Update(&c, str, 512);
-        } else {
-            MD5_Update(&c, str, length);
-        }
-        length -= 512;
-        str += 512;
+  while (length > 0) {
+    if (length > 512) {
+      MD5_Update(&c, str, 512);
+    } 
+    else {
+      MD5_Update(&c, str, length);
     }
+    length -= 512;
+    str += 512;
+  }
 
-    MD5_Final(digest, &c);
+  MD5_Final(digest, &c);
 
-    for (n = 0; n < 16; ++n) {
-        snprintf(&(out[n*2]), 16*2, "%02x", (unsigned int)digest[n]);
-    }
+  for (n = 0; n < 16; ++n) {
+    snprintf(&(out[n*2]), 16*2, "%02x", (unsigned int)digest[n]);
+  }
 
-    return out;
+  return out;
 }
