d8258176833d2c0b5995229639a7007e9f0368ad
Wayne Booth <wbooth@clearscene.net>
2013-05-17 07:52:36 +0100

    add new backpopulate test

---
+++ test/i/3_h_backpopulate/1.sql
@@ -0,0 +1,8 @@
+BEGIN TRANSACTION;
+UPDATE config SET config_value='yes' WHERE config_option='backpopulate_phash';
+INSERT INTO docs (docid, doneocr, ocrtext, depth, lines, ppl, resolution, docdatey, docdatem, docdated, entrydate, filetype, title, pages, actionrequired, hardcopyKept, image_phash) VALUES (1,1,'This is the OCR text.',8,3509,2519,300,2010,12,31,'2011-01-02T21:13:04.393946Z',2,'Test Titletext',1,0,0,0);
+INSERT INTO docs (docid, doneocr, ocrtext, depth, lines, ppl, resolution, docdatey, docdatem, docdated, entrydate, filetype, title, pages, actionrequired, hardcopyKept, image_phash) VALUES (4,1,'This is OCR text 4.',8,3509,2519,300,2010,12,30,'2011-01-03T21:13:04.393946Z',4,'Test 1 TitleText 2',1,0,0,0);
+INSERT INTO docs (docid, doneocr, ocrtext, depth, lines, ppl, resolution, docdatey, docdatem, docdated, entrydate, filetype, title, pages, actionrequired, hardcopyKept, image_phash) VALUES (3,1,'This is OCR text 2.',8,3509,2519,300,2011,01,01,'2011-01-04T21:13:04.393946Z',2,'Test 2 Title text',1,1,0,0);
+INSERT INTO docs (docid, doneocr, ocrtext, depth, lines, ppl, resolution, docdatey, docdatem, docdated, entrydate, filetype, title, pages, actionrequired, hardcopyKept, image_phash) VALUES (2,1,'This is final OCR text 3.',8,3509,2519,300,2012,12,31,'2011-01-04T21:13:05.393946Z',3,'Test 3 Title',1,1,0,999);
+INSERT INTO docs (docid, doneocr, ocrtext, depth, lines, ppl, resolution, docdatey, docdatem, docdated, entrydate, filetype, title, pages, actionrequired, hardcopyKept, image_phash) VALUES (5,1,'fantastical',8,3509,2519,300,2039,01,01,'2014-01-04T21:13:05.393946Z',1,NULL,1,0,0,999);
+COMMIT;
