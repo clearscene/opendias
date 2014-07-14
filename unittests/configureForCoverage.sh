4bb46fa19846020e5e4fce98fb1c5e6e69ec4e20
Wayne Booth <wbooth@clearscene.net>
2014-07-14 20:26:56 +0100

    split the 'unit tests converage' script into two parts

--- unittests/configureForCoverage.sh
+++ unittests/configureForCoverage.sh
@@ -4,8 +4,4 @@ rm -rf test/results/coverage/*
 rm -rf src/*.gcda src/*.gcno
 ./configure CFLAGS=' -g -O --coverage' LIBS='-lgcov'
 make
-lcov -c -i -d src -o test/results/coverage/app_base.info
-make check
-lcov -c -d src -o test/results/coverage/app_test.info
-lcov -a test/results/coverage/app_base.info -a test/results/coverage/app_test.info -o test/results/coverage/app_total.info 
-genhtml -o test/results/coverage test/results/coverage/app_total.info 
+echo Now run 'unittests/runForCoverage.sh'
---
+++ unittests/runForCoverage.sh
@@ -0,0 +1,9 @@
+
+rm -rf test/results/coverage/*
+rm -rf src/*.gcda src/*.gcno
+lcov -c -i -d src -o test/results/coverage/app_base.info
+make check
+lcov -c -d src -o test/results/coverage/app_test.info
+lcov -a test/results/coverage/app_base.info -a test/results/coverage/app_test.info -o test/results/coverage/app_total.info 
+genhtml -o test/results/coverage test/results/coverage/app_total.info 
+echo Now check 'test/results/coverage/' for coverage results.
