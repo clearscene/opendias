f831d2f4a99d8cc5df12e822cb7aaf5aef1d592c
Wayne Booth <wbooth@clearscene.net>
2014-08-04 17:40:08 +0100

    actually log what the app would log in the methods being unit tested

--- unittests/stubbed_debug.c
+++ unittests/stubbed_debug.c
@@ -16,13 +16,71 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "../src/utils.h"
 #include "../src/debug.h"
 
 int trigger_log_verbosity( const int verbosity ) {
-  return 0;
+  return 1;
+}
+
+void i_o_log(const char *file, const int line, const int verbosity, const char *message, va_list inargs) {
+
+  va_list ap;
+  va_copy(ap,inargs);
+
+  FILE *fp;
+  char *thumb = o_strdup("%s:%s:%s:%d ");
+
+  char *ltime = getTimeStr();
+  char *vb;
+  if(verbosity == 1) {
+    vb = o_strdup("ERR");
+  }
+  else if(verbosity == 2) {
+    vb = o_strdup("WRN");
+  }
+  else if(verbosity == 3) {
+    vb = o_strdup("INF");
+  }
+  else if(verbosity == 4) {
+    vb = o_strdup("DBG");
+  }
+  else if(verbosity == 5) {
+    vb = o_strdup("SQL");
+  }
+  else 
+    vb = o_strdup("---");
+
+  if((fp = fopen("/tmp/unit-test-logs.log", "a"))==NULL) {
+    fprintf(stderr,"Cannot open log file.\n");
+    exit(1);
+  }
+
+  fprintf(fp,thumb,ltime,vb,file,line);
+  vfprintf(fp,message,inargs);
+  fprintf(fp,"\n");
+  fflush(fp);
+
+  fclose(fp);
+  free(ltime);
+  free(thumb);
+  free(vb);
+
 }
 
 extern void oo_log(const char *file, const int line, const int verbosity, const char *message, ... ) {
+
+  if( trigger_log_verbosity( verbosity ) ) {
+    va_list inargs;
+    va_start(inargs, message);
+    i_o_log(file, line, verbosity, message, inargs);
+    va_end(inargs);
+  }
 
 }
 
